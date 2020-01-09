5404ca94a2f62c01ba884dcfc4329c86bee695be
sl23 <seanli.1996@gmail.com>
2020-01-09 16:11:04 -0500

    Website functionality has been implemented, additional styling work still WIP

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ $(function() {
     let articleURL = `https://api.nytimes.com/svc/topstories/v2/${category}.json`;
     //https://api.nytimes.com/svc/topstories/v2/{section}.json
     console.log(articleURL);
+
+    // Clears article area
     $(".articleDisplay").empty();
+
+    // code for changing header classes & css settings
+    $("header").addClass("headerLoading");
+
+    // $(".logoImage")
+    //   .addClass("logoImageSmall")
+    //   .removeClass("logoImage");
+    $(".loadingGIF").css("display", "flex");
+
+    // API & Data display functions
     $.ajax({
       method: "GET",
       url: articleURL,
@@ -18,36 +30,47 @@ $(function() {
     }).done(function(data) {
       console.log(data);
       console.log(data.results);
+      $(".articleDisplay").css("display", "grid");
+      $("header")
+        .addClass("headerSmall")
+        .removeClass("headerBig");
       //To filter out articles with no images, create an if statement to check if multimedia array is empty.
 
       // for loop to iterate through top 12 articles
       let withImg = 0;
       for (let i = 0; withImg < 12; i++) {
-        let cssID = 0;
-        console.log(data.results[i]);
-        console.log(data.results[i].abstract);
-        console.log(data.results[i].multimedia[0] === undefined);
+        // console.log(data.results[i]);
+        // console.log(data.results[i].abstract);
+        // console.log(data.results[i].multimedia[0] === undefined);
 
-        if (data.results[i].multimedia[0] === undefined) {
-          withImg = withImg;
-        } else {
-          let cssID = `article${withImg}`;
-          let imgURL = data.results[i].multimedia[3].url;
+        if (data.results[i].multimedia[0] !== undefined) {
+          let imgURL = data.results[i].multimedia[4].url;
           let redirectUrl = data.results[i].url;
           console.log(imgURL);
-          console.log(cssID);
           $(".articleDisplay").append(`<div class = article> 
-          <a href = "${redirectUrl}"><img id = articleImages src=${imgURL}></a>
-          <p class = ${cssID} id = articleAbstracts>${data.results[i].abstract}</p>
-            <div>`);
-          //   $(`.${cssID}`).append(`<img id = articleImages src=${imgURL}>`);
-          //   $(`.${cssID}`).append(
-          //     `<p class = ${cssID} id = articleAbstracts>${data.results[i].abstract}</p>`
-          //   );
-          withImg = withImg + 1;
+          <a href = "${redirectUrl}"><img class = articleImages src=${imgURL}></a>
+          <p class = articleAbstracts>${data.results[i].abstract}</p>
+            </div>`);
+          withImg++;
+          console.log(withImg);
         }
-        // $(".articleDisplay").append(``);
       }
+      $(".loadingGIF").css("display", "none");
+      $("header").removeClass("headerLoading");
     });
   });
+
+  $(".article").hover(function() {
+    console.log("article Display Hover");
+  });
+
+  $(".articleImages").hover(
+    function() {
+      console.log("hoverin");
+    },
+    function() {
+      console.log("hoverout");
+    }
+  );
+  // on click for css class not working. Same thing for hover
 });
